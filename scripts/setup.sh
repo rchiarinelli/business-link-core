2e9597d831939eb8ed05053b0a2121f1e12da3c3
Rafael Chiarinelli <rchiarinelli@gmail.com>
2022-01-03 21:34:40 +0000

    Config to enable docker service

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,1 @@
-sudo yum update -y
-sudo yum install docker -y
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o/usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo yum install ruby -y
+systemctl restart docker
