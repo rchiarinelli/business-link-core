4c76320d6cb7d64aea1f94f9a9daf0db97a3c3ec
Rafael Chiarinelli <rchiarinelli@gmail.com>
2022-01-03 15:23:30 +0000

    Fix at docker-compose setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,3 +3,5 @@ sudo yum install docker -y
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o/usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo yum install ruby -y
+chmod +x ./install
+sudo ./install auto
