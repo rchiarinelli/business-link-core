29b1193add2755429dbf9bddd2fb275d8fdbc0e4
Rafael Chiarinelli <rchiarinelli@gmail.com>
2022-01-03 11:37:41 +0000

    Added docker-compose setup for codedeploy

---
+++ scripts/setup.sh
@@ -0,0 +1,5 @@
+sudo yum update -y
+sudo yum install docker -y
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o/usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo yum install ruby -y
