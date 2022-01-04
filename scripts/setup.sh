14f9430963a82cf0c095b445275cfff9a54de7ed
Rafael Chiarinelli <rchiarinelli@gmail.com>
2022-01-04 08:36:10 +0000

    more debug info

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,3 +3,6 @@ yum install -y python-pip
 pip install docker-compose
 service docker stop
 service docker start
+ls -l /var/app
+docker version
+docker-compose --version
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,6 @@
-ls -l /var/app
+echo Docker version
 docker version
+echo Docker-compose version
 docker-compose --version
+echo running containers
 docker-compose -f /var/app/docker-compose.yaml up -d
