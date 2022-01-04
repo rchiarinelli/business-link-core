dce4aa35d027b0d534aa18cc5848c1d70d9e3321
Rafael Chiarinelli <rchiarinelli@gmail.com>
2022-01-04 09:26:24 +0000

    wrong char at debug echo statements

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,18 +1,18 @@
-echo >>> install docker
+echo '********' install docker
 yum install docker -y
-echo >>> install pip
+echo '********' install pip
 yum install -y python-pip
-echo >>> forcing pip downgrade
+echo '********' forcing pip downgrade
 pip install --upgrade --force-reinstall pip==9.0.3
-echo >>> installing docker compose
+echo '********' installing docker compose
 pip install docker-compose
 echo restarting docker services
 systemctl start docker
 systemctl enable docker
 systemctl restart docker
-echo >>> Docker version
+echo '********' Docker version
 docker version
-echo >>> Docker-compose version
+echo '********' Docker-compose version
 docker-compose --version
-echo >>> running containers
+echo '********' running containers
 docker-compose -f /var/app/docker-compose.yaml up -d
