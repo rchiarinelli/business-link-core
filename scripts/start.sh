07d161297d756eba799a9fbae5f0a202cb8e4b46
Rafael Chiarinelli <rchiarinelli@gmail.com>
2022-01-04 09:47:20 +0000

    removed pip installation

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,3 @@
-echo  install pip
-yum install -y python-pip
-echo  forcing pip downgrade
-pip install --upgrade --force-reinstall pip==9.0.3
 echo  installing docker compose
 pip install docker-compose
 echo  restarting docker services
