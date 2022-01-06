1411272a6703dd4a6176c51e39136ae036c18f69
Rafael Chiarinelli <rchiarinelli@gmail.com>
2022-01-06 09:39:46 +0000

    Added --force-recreate to make docker download the newest images

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,4 +4,4 @@ docker version
 echo  Docker-compose version
 docker-compose --version
 echo  running containers
-docker-compose -f /var/app/docker-compose.yaml up -d
+docker-compose -f /var/app/docker-compose.yaml up -d --force-recreate
